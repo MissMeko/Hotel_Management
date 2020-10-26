748d43e2812d1b7d0fcf3e115083bfc691507062
Meko <47412468+MissMeko@users.noreply.github.com>
2020-10-26 11:08:05 +0200

    set guest booking template

--- hotel_Management/src/app/public/home/home.page.ts
+++ hotel_Management/src/app/public/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserModel } from '../../model/userModel';
+import { GeneralService } from '../../services/general.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -8,23 +9,18 @@ import { UserModel } from '../../model/userModel';
 })
 export class HomePage implements OnInit {
   imageList = [
-    'assets/img/hotels/hotel_1.jpg',
-    'assets/img/hotels/hotel_1_2.jpg',
-    'assets/img/hotels/hotel_1_3.jpg',
-    'assets/img/hotels/hotel_1_4.jpg',
-    'assets/img/hotels/hotel_1_5.jpg',
-    'assets/img/hotels/hotel_1_6.jpg',
-    'assets/img/hotels/hotel_1.jpg',
-    'assets/img/hotels/hotel_1_2.jpg',
-    'assets/img/hotels/hotel_1_3.jpg',
-    'assets/img/hotels/hotel_1_4.jpg',
-    'assets/img/hotels/hotel_1_5.jpg',
-    'assets/img/hotels/hotel_1_6.jpg',
+    {showBookingForm: false, src: 'assets/img/hotels/hotel_1.jpg'},
+    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_2.jpg'},
+    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_4.jpg'},
+    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_5.jpg'},
+    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_6.jpg'}
   ];
   user: UserModel;
+  bookingFormFlag: boolean;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private generalService: GeneralService,
   ) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
@@ -35,6 +31,16 @@ export class HomePage implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  showBookingForm(room){
+    room.showBookingForm = !room.showBookingForm;
+  }
+
+  processBooking(item){
+    this.generalService.presentBookingConfirmation("Room Booked", "Success").then(() => {
+      this.showBookingForm(item);
+    });
   }
 
 }
