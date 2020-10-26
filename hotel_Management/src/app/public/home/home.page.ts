f4f7914d416760cc26b7eff0b3bdd0832e50fc6a
Meko <47412468+MissMeko@users.noreply.github.com>
2020-10-26 14:04:45 +0200

    calculate price code

--- hotel_Management/src/app/public/home/home.page.ts
+++ hotel_Management/src/app/public/home/home.page.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserModel } from '../../model/userModel';
+import { RoomModel, UserModel } from '../../model/userModel';
 import { GeneralService } from '../../services/general.service';
+import { RoomService } from '../../services/room.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  imageList = [
-    {showBookingForm: false, src: 'assets/img/hotels/hotel_1.jpg'},
-    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_2.jpg'},
-    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_4.jpg'},
-    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_5.jpg'},
-    {showBookingForm: false, src: 'assets/img/hotels/hotel_1_6.jpg'}
-  ];
+  // imageList = [
+  //   { showBookingForm: false, src: 'assets/img/hotels/hotel_1.jpg' },
+  //   { showBookingForm: false, src: 'assets/img/hotels/hotel_1_2.jpg' },
+  //   { showBookingForm: false, src: 'assets/img/hotels/hotel_1_4.jpg' },
+  //   { showBookingForm: false, src: 'assets/img/hotels/hotel_1_5.jpg' },
+  //   { showBookingForm: false, src: 'assets/img/hotels/hotel_1_6.jpg' }
+  // ];
   user: UserModel;
+  allRooms: RoomModel[];
+  bookedRooms: RoomModel[];
+  currentRooms: RoomModel[];
+  availableRooms: RoomModel[];
+
   bookingFormFlag: boolean;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private roomService: RoomService,
     private generalService: GeneralService,
   ) {
     this.route.queryParams.subscribe(params => {
@@ -27,19 +34,51 @@ export class HomePage implements OnInit {
         this.user = this.router.getCurrentNavigation().extras.state.userInfo;
       }
       console.log(this.user);
+      this.refreshRooms();
     });
-   }
+  }
+
+  refreshRooms(){
+    this.roomService.getAllRooms().then(response => {
+      console.log(response);
+      this.availableRooms = response.filter(element => element.userId === '');
+      this.bookedRooms = response.filter(element => element.userId !== '');
+      this.availableRooms.forEach(room => {
+        room.showBookingForm = false;
+      });
+      console.log(this.availableRooms);
+    }).catch(error => {
+      console.dir(error);
+    });
+  }
 
   ngOnInit() {
   }
 
-  showBookingForm(room){
+  showBookingForm(room) {
     room.showBookingForm = !room.showBookingForm;
   }
 
-  processBooking(item){
-    this.generalService.presentBookingConfirmation("Room Booked", "Success").then(() => {
-      this.showBookingForm(item);
+  calculateTotalPrice(item: RoomModel) {
+
+  }
+
+  processBooking(item: RoomModel) {
+    console.log(item);
+    const startDate = new Date(item.startDate);
+    const endDate = new Date(item.endDate);
+    const numDays = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
+    console.log(numDays.toFixed(0));
+    item.totalPricing = item.price * parseInt(numDays.toFixed(0));
+    item.userId = this.user.id;
+    this.roomService.editRoom(item).then(response => {
+      this.generalService.presentBookingConfirmation(`Room Booked for ${numDays.toFixed(0)} days. \n
+       Total Price: R${item.totalPricing.toFixed(2)}`, "Success").then(() => {
+        this.showBookingForm(item);
+        this.refreshRooms();
+      });
+    }).catch(error => {
+      this.generalService.presentPopup("Failed to book room");
     });
   }
 
