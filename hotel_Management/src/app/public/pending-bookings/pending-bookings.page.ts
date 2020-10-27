902f91c949613726202b3f0ed113fed4ab58c266
Meko <47412468+MissMeko@users.noreply.github.com>
2020-10-27 19:09:56 +0200

    pending bookings code update

--- hotel_Management/src/app/public/pending-bookings/pending-bookings.page.ts
+++ hotel_Management/src/app/public/pending-bookings/pending-bookings.page.ts
@@ -13,6 +13,9 @@ import { AuthenticationService } from '../../services/authentication.service';
 export class PendingBookingsPage implements OnInit {
   bookedRooms: RoomModel[];
   currentUser: UserModel;
+  isCheckIn: boolean;
+  searchTerm: string = '';
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -31,7 +34,14 @@ export class PendingBookingsPage implements OnInit {
   refreshRooms() {
     this.roomService.getAllRooms().then(response => {
       console.log(response);
-      this.bookedRooms = response.filter(element => element.userId === this.currentUser.id);
+      if (this.currentUser.userType === 'cleck') {
+        console.log('cleck');
+        this.bookedRooms = response.filter(element => element.userId !== '');
+      } else {
+        console.log('guest');
+        this.bookedRooms = response.filter(element => element.userId === this.currentUser.id)
+        .filter(element => element.checkIn === false);
+      }
       this.bookedRooms.forEach(room => {
         room.showBookingForm = false;
       });
@@ -56,7 +66,7 @@ export class PendingBookingsPage implements OnInit {
     });
   }
 
-  editBooking(room: RoomModel){
+  editBooking(room: RoomModel) {
     this.roomService.editRoom(room).then(res => {
       this.generalService.presentPopup('Booking edited', 'Success').then(() => {
         this.refreshRooms();
@@ -65,6 +75,44 @@ export class PendingBookingsPage implements OnInit {
       console.dir(error);
       this.generalService.presentPopup('Failed to edit booking', 'Error');
     });
+  }
+
+  processRoomCheckin(room: RoomModel, reservation: boolean) {
+    let popupMessage = '';
+    if (reservation) {
+      this.isCheckIn = true;
+      popupMessage = `Room ${room.number} checked in`;
+    } else {
+      room.userId = '';
+      this.isCheckIn = false;
+      popupMessage = `Room ${room.number} checked out`;
+    }
+    room.checkIn = reservation;
+
+    this.roomService.editRoom(room).then(res => {
+      this.generalService.presentPopup(popupMessage, 'Success').then(() => {
+        this.refreshRooms();
+      });
+    }).catch(error => {
+      console.log(error);
+      this.generalService.presentPopup('Failed to edit booking', 'Error');
+    });
+  }
+
+  setFilteredItems() {
+    console.log(this.bookedRooms);
+    const tempArray = [];
+    this.bookedRooms.forEach(room => {
+      if (room.roomType && room.roomType.indexOf(this.searchTerm) !== -1) {
+        tempArray.push(room);
+      }
+    });
+    this.bookedRooms = tempArray;
+    if (this.searchTerm === '') {
+      this.generalService.present();
+      this.refreshRooms();
+      this.generalService.dismiss();
+    }
   }
 
 }
